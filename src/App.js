a03b957dc4880053a9e02ae4948b760234202d30
leeway01 <kizaruk01@gmail.com>
2025-02-04 22:57:03 +0900

    logo add

--- src/App.js
+++ src/App.js
@@ -4,10 +4,19 @@ import WorldMap from './components/WorldMap';
 function App() {
   return (
     <div>
-      <h1>Where the Animals Live</h1>
+      <img
+        src="/assets/Where_the_Animals_Live-logo.png"
+        alt="Where the Animals Live"
+        style={logoStyle}
+      />
       <WorldMap />
     </div>
   );
 }
+
+const logoStyle = {
+  width: '450px', // 로고 크기 조정
+  height: 'auto', // 비율 유지
+};
 
 export default App;
